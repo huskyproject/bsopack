9e03fec45cad0c3e43ae3d23f9780b3c5b159dee
d.sergienko <>
2001-03-19 20:27:51 +0000

    removing old files...

--- bsoutil.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BSOUTIL_H_
-#define _BSOUTIL_H_
-
-void packNetMailForLink(s_link *link);
-
-#endif
--- log.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LOG_H
-#define _LOG_H
-
-void Log(char level, char *msg,...);
-
-#endif
