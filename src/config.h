0c3d45f779f8e8c5f66303cbcf6a4235d3dae011
sfpavel <>
2003-02-11 07:38:11 +0000

    Update version to 1.4.0, New debian package

--- src/config.h
+++ src/config.h
@@ -4,9 +4,9 @@
 #define _CONFIG_H
 
 #define VER_MAJOR 1
-#define VER_MINOR 3
+#define VER_MINOR 4
 #define VER_PATCH 0
-#define VER_BRANCH BRANCH_CURRENT
+#define VER_BRANCH BRANCH_STABLE
 
 #include <fidoconf/fidoconf.h>
 
