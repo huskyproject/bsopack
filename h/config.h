661559a6c65d0380d16ddd024be8878d6e14e5b1
Andy U. Luk'yanov <andr_lukyanov@users.sourceforge.net>
2003-08-05 01:00:06 +0000

    Moved *.h to ./h

--- h/config.h
+++ h/config.h
@@ -3,22 +3,14 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-#define VER_MAJOR 1
-#define VER_MINOR 9
-#define VER_PATCH 0
-#define VER_BRANCH BRANCH_CURRENT
-
 #include <fidoconf/fidoconf.h>
 
 #define MAXPATH 256
 
 
-extern s_fidoconfig *fidoConfig;
+extern s_fidoconfig *config;
 extern char *logFileName;
-extern int enable_quiet;
-extern int enable_debug;
 extern char *fidoConfigFile;
-extern char *VERSION;
 
 void getConfig();
 void freeConfig();
